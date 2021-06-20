964b96b6b7ca19597653debefb3eb26abacbb97f
Viacheslav Andreev <maxivimax@icloud.com>
2021-06-20 18:33:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,13 @@ const fetch = require("isomorphic-unfetch");
 const { promises: fs } = require("fs");
 const path = require("path");
 var weather = require('openweather-apis');
-const un = process.env.USERNAME;
 
 weather.setLang('ru');
 weather.setCity('Fairplay');
 weather.setUnits('metric');
 weather.setAPPID("a2d8addaa9d3224d5b808a9dd5a5110c");
 
-const myghurl = "https://api.github.com/users/";
+const myghurl = "https://api.github.com/users/maxivimax";
 
 async function main() {
   var sjsontemp = weather.getSmartJSON();
@@ -17,8 +16,6 @@ async function main() {
   const readmeTemplate = (
     await fs.readFile(path.join(process.cwd(), "./README.template.md"))
   ).toString("utf-8");
-
-  myghurl = myghurl + un;
 
   const info = await (
     await fetch(myghurl, {})
